370130f60f3b60608972a9d05d0e6a3df787cef9
shounak03 <shounakmis.3@gmail.com>
2024-09-24 18:07:42 +0530

    courses-part coded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 
 
@@ -7,6 +8,7 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
 
       <HeroSection/>
+      <FeaturedCourses/>
     </main>
 
   );
